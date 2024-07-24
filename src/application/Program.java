72d8c5401567dca12c10a52f247348c99ffc7da9
rfurlani <furlani.roberto@gmail.com>
2024-07-24 19:13:33 -0300

    Criada SellerDaoJDBC que implementa a interface SellerDao e a FactoryDao que chama as implementações por debaixo dos panos

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,10 @@ public class Program {
 			Department obj = new Department(1, "Books");
 			System.out.println(obj);
 			SimpleDateFormat stf = new SimpleDateFormat("dd/MM/yyyy");
-			Seller obj2 = new Seller(1, "Bob", "Bob@gmail.com", new java.sql.Date(stf.parse("24/03/1963").getTime()), 4500.00, obj);
+			Seller obj2 = new Seller(1, "Bob", "Bob@gmail.com", new java.sql.Date(stf.parse("24/03/1963").getTime()), 4500.00, objs);
+			
+			SellerDao sellerdao = DaoFactory.createSellerDao();
+			
 			System.out.println(obj2);
 		}
 		catch (ParseException e) {
