d33d03bc788098b16a48589217b1bbc1849afea5
rfurlani <furlani.roberto@gmail.com>
2024-07-24 18:48:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,24 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		try {
+			Department obj = new Department(1, "Books");
+			System.out.println(obj);
+			SimpleDateFormat stf = new SimpleDateFormat("dd/MM/yyyy");
+			Seller obj2 = new Seller(1, "Bob", "Bob@gmail.com", new java.sql.Date(stf.parse("24/03/1963").getTime()), 4500.00, obj);
+			System.out.println(obj2);
+		}
+		catch (ParseException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 
 	}
 
