dcb9080efa06e6ab101324aadd16e3a777ab95f7
rfurlani <furlani.roberto@gmail.com>
2024-07-25 11:46:02 -0300

    Immplementando delete Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -9,9 +10,9 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-
+	
 	public static void main(String[] args) {
-			
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 			
 		Seller seller = sellerDao.findById(3);
@@ -42,6 +43,13 @@ public class Program {
 		seller.setName("Brenda Lee");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println();
+		System.out.println("====> Test 6 Seller delete <====");
+		System.out.print("Entre com o ID do vendedor que será deletado: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Seller deleted!");
 	}
 
 }
