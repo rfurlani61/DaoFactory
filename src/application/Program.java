b5f1abcbcbe27d21856dce66e8e8c170e7412223
rfurlani <furlani.roberto@gmail.com>
2024-07-25 11:32:04 -0300

    Immplementando update Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,17 @@ public class Program {
 			System.out.println(s);
 		}
 		System.out.println();
-		System.out.println("====> Test 4 Selleer insert <====");
+		System.out.println("====> Test 4 Seller insert <====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id: " + newSeller.getId());
+		
+		System.out.println();
+		System.out.println("====> Test 5 Seller update <====");
+		seller = sellerDao.findById(1);
+		seller.setName("Brenda Lee");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
